a1c79724baa05a962923a36ba113769490f50572
seokhyun Yoon <gah1234@naver.com>
2022-11-06 00:11:39 +0900

    best item service

--- 슬라이드배너/test.js
+++ 슬라이드배너/test.js
@@ -1,10 +1,13 @@
 /*
   div사이즈 동적으로 구하기
 */
+const index_now = document.querySelector('.index_now');
+const index_all = document.querySelector('.index_all');
 const outer = document.querySelector('.outer');
 const innerList = document.querySelector('.inner-list');
 const inners = document.querySelectorAll('.inner');
 const imgs = document.querySelectorAll('img');
+index_all.innerHTML = `${inners.length}`;
 let currentIndex = 0; // 현재 슬라이드 화면 인덱스
 
 inners.forEach((inner) => {
@@ -24,6 +27,7 @@ const buttonLeft = document.querySelector('.button-left');
 const buttonRight = document.querySelector('.button-right');
 
 buttonLeft.addEventListener('click', () => {
+  Indexvalue();
   currentIndex--;
   currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
   innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
@@ -32,22 +36,27 @@ buttonLeft.addEventListener('click', () => {
 });
 
 buttonRight.addEventListener('click', () => {  
+  Indexvalue();
   currentIndex++;
   currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
   innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
   clearInterval(interval); // 기존 동작되던 interval 제거
   interval = getInterval(); // 새로운 interval 등록
 });
-
+function Indexvalue(){
+  index_now.innerHTML = `${currentIndex}`;
+  index_all.innerHTML = `${inners.length}`;
+}
 /*
   주기적으로 화면 넘기기
 */
-// const getInterval = () => {
-//   return setInterval(() => {
-//     currentIndex++;
-//     currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
-//     innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
-//   }, 2000);
-// }
+const getInterval = () => {
+  return setInterval(() => {
+    Indexvalue();
+    currentIndex++;
+    currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+  }, 2000);
+}
 
-// let interval = getInterval(); // interval 등록
+let interval = getInterval(); // interval 등록
