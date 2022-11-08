95777373682711fb8985bdb7917473347b66f68a
seokhyun Yoon <gah1234@naver.com>
2022-11-09 00:14:10 +0900

    category page

--- category/category.js
+++ category/category.js
@@ -2,7 +2,6 @@ const bottomVector = document.querySelector(".bottomVector");
 const nav_realtimesearch = document.querySelector(".nav_realtimesearch");
 const input_search = document.querySelector(".input_search");
 const recentSearchContainer = document.querySelector(".recentSearchContainer");
-const categoryClick = document.querySelector(".categoryClick");
 let view = "visible"
 function realtimeViewVisible(e) {
   console.log(e);
@@ -27,17 +26,6 @@ function searchViewVisible(e) {
         view = "visible";
     }   
 }
-function categorybox(e){
-  if (view === "visible") {
-    categoryClick.style="visibility:visible"
-      view = "hidden";
-  }
-  else{
-    categoryClick.style = "visibility:hidden"
-      view = "visible";
-  }  
-}
-
 /*
   div사이즈 동적으로 구하기
 */
@@ -50,8 +38,7 @@ const imgs = document.querySelectorAll('.slideimg');
 const circle = document.querySelector(".circle");
 let currentIndex = 0; // 현재 슬라이드 화면 인덱스
 
-function circleMove(item) {
-  console.log(item);
+function circleMove(e) {
     if (view === "visible") {
       circle.style="animation-name:circleleft";
       circle.style=`transform:translate(-18px,0)`;
@@ -64,59 +51,6 @@ function circleMove(item) {
     }   
 }
 
-
-
-inners.forEach((inner) => {
-  inner.style.width = `${outer.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
-})
-
-imgs.forEach((img) => {
-    img.style.width = `${outer.clientWidth}px`;
-    img.style.height = `${outer.clientHeight}px`;
-})
-innerList.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
-
-/*
-  버튼에 이벤트 등록하기
-*/
-const buttonLeft = document.querySelector('.button-left');
-const buttonRight = document.querySelector('.button-right');
-
-buttonLeft.addEventListener('click', () => {
-  currentIndex--;
-  currentIndex = currentIndex < 0 ? 0 : currentIndex; // index값이 0보다 작아질 경우 0으로 변경
-  Indexvalue();
-  innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
-  clearInterval(interval); // 기존 동작되던 interval 제거
-  interval = getInterval(); // 새로운 interval 등록
-});
-
-buttonRight.addEventListener('click', () => {  
-  currentIndex++;
-  currentIndex = currentIndex >= inners.length ? inners.length - 1 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 마지막 인덱스값으로 변경
-  Indexvalue();
-  innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
-  clearInterval(interval); // 기존 동작되던 interval 제거
-  interval = getInterval(); // 새로운 interval 등록
-});
-function Indexvalue(){
-    index_now.innerHTML = `${currentIndex}`;
-    index_all.innerHTML = `${inners.length}`;
-  }
-/*
-  주기적으로 화면 넘기기
-*/
-const getInterval = () => {
-  return setInterval(() => {
-    currentIndex++;
-    Indexvalue();
-    currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
-    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
-  }, 2000);
-}
-
-let interval = getInterval(); // interval 등록
-
 //페이지네이션 ----------------------------------------------------------------------
 const rowsPerPage = 8;
 const rows = document.querySelectorAll('.bestitem');
@@ -126,11 +60,12 @@ const numbers = document.querySelector('#numbers');
 
 const prevPageBtn = document.querySelector('.prevPageBtn');
 const nextPageBtn = document.querySelector('.nextPageBtn');
+console.log(prevPageBtn);
 let pageActiveIdx = 0; //현재 보고 있는 페이지 그룹 번호
-let currentPageNum = 0; //현재 보고 있는 페이지네이션 번호
+let currentPageNum = 0; //현재 보고 있는 페이지 번호
 let maxPageNum = 9; //페이지그룹 최대 개수
 console.log(numbers);
-for(let i=1; i<pageCount.length; i++){
+for(let i=1; i<pageCount; i++){
    numbers.innerHTML += `<li><a href="">${i}</a></li>`;
 }
 
@@ -148,7 +83,6 @@ numberBtn.forEach((item,idx) => {
         nb.classList.remove('active');
       }
       e.target.classList.add('active');
-  
       displayRow(idx);
     });
 });
@@ -169,7 +103,8 @@ function displayRow(idx){
   for(nb of numberBtn){
     nb.classList.remove('active');
   }
-  e.target.classList.add('active');
+  console.log(numberBtn[idx]);
+  numberBtn[idx].classList.add('active');
 }
 
 displayRow(0);
@@ -181,10 +116,11 @@ function displayPage(num){
     nb.style.display ='none';
   }
   let totalPageCount = Math.ceil(pageCount/maxPageNum);
-
+  console.log(totalPageCount);
+  console.log(pageActiveIdx);
   let pageArr = [...numberBtn];
   let start = num*maxPageNum;
-  let end = num+maxPageNum;
+  let end = start+maxPageNum;
   let pageListArr = pageArr.slice(start, end);
 
   for(let item of pageListArr){
@@ -194,11 +130,25 @@ function displayPage(num){
   if(pageActiveIdx == 0) {
     prevPageBtn.style.display = 'none'
   } else {
-    prevPageBtn.style.display = 'block'
+    prevPageBtn.style.display = 'flex'
   }
   if(pageActiveIdx == totalPageCount -1) {
-    prevPageBtn.style.display = 'none'
+    nextPageBtn.style.display = 'none'
   } else {
-    prevPageBtn.style.display = 'block'
+    nextPageBtn.style.display = 'flex'
   }
 }
+displayPage(0);
+
+nextPageBtn.addEventListener('click', ()=>{
+  let nextPageNum = pageActiveIdx*maxPageNum+maxPageNum;
+  displayRow(nextPageNum);
+  ++pageActiveIdx;
+  displayPage(pageActiveIdx);
+});
+prevPageBtn.addEventListener('click', ()=>{
+  let nextPageNum = pageActiveIdx*maxPageNum-maxPageNum;
+  displayRow(nextPageNum);
+  --pageActiveIdx;
+  displayPage(pageActiveIdx);
+});
