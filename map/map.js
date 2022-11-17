a8c098c0254883d537186e7db5fa9c0ce0e3f639
seokhyun Yoon <gah1234@naver.com>
2022-11-17 13:26:44 +0900

    realtimesearch 적용

--- map/map.js
+++ map/map.js
@@ -50,6 +50,28 @@ function circleMove(e) {
 
     }   
 }
+const outers = document.querySelector('.nav_realmenu_box');
+const innerLists = document.querySelector('.nav_realmenu_left');
+const innerss = document.querySelectorAll('.nav_inner');
+// const imgs = document.querySelectorAll('img');
+let currentIndexs = 0; // 현재 슬라이드 화면 인덱스
+
+innerss.forEach((inner) => {
+    inner.style.width = `${innerLists.clientWidth}px`; // inner의 width를 모두 outer의 width로 만들기
+    inner.style.height = `${innerLists.clientHeight}px`;
+})
+innerLists.style.height = `${outers.clientHeight * innerss.length}px`;
+
+
+const getIntervals = () => {
+    return setInterval(() => {
+        currentIndexs++;
+        currentIndexs = currentIndexs >= innerss.length ? 0 : currentIndexs;
+        innerLists.style.marginTop = `-${outers.clientHeight * currentIndexs}px`;
+    }, 4000);
+}
+
+let intervalz = getIntervals(); // interval 등록
 
 //페이지네이션 ----------------------------------------------------------------------
 const rowsPerPage = 8;
